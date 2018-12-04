16a9ab298218827977b6cddceb3a4fab83efea73
The Anh <theanh0512@gmail.com>
2018-12-04 23:13:00 +0800

    try battery

--- app/features/map/components/Main.js
+++ app/features/map/components/Main.js
@@ -1,47 +1,102 @@
 import React from 'react';
-import { Dimensions, View } from 'react-native';
-import NavigationBar from 'app/components/NavigationBar';
+import _ from 'lodash';
+import { Constants, Location, Permissions } from 'expo';
+import { ActivityIndicator, Alert, Dimensions, Platform, View } from 'react-native';
+import geolib from 'geolib';
 import { connect } from 'react-redux';
+import NavigationBar from 'app/components/NavigationBar';
 import { getScooters } from '../actions/ScooterListAction';
 import ScootersMapView from 'app/components/ScootersMapView';
 import Colors from 'app/common/Colors';
-import _ from 'lodash';
 
 const { height } = Dimensions.get('window');
 
 export default class Main extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      coords: null,
+      errorMessage: null,
+    };
+  }
+
+  componentWillMount() {
+    if (Platform.OS === 'android' && !Constants.isDevice) {
+      this.setState({
+        errorMessage: 'Oops, this will not work on Sketch in an Android emulator. Try it on your device!',
+      });
+    } else {
+      this._getLocationAsync();
+    }
+  }
+
+  _getLocationAsync = async () => {
+    let { status } = await Permissions.askAsync(Permissions.LOCATION);
+    if (status !== 'granted') {
+      this.setState({
+        errorMessage: 'Permission to access location was denied',
+      });
+      Alert.alert(
+        'Permission Denied',
+        'Permission to access location was denied. Please go to setting to enable Location Access for Expo.',
+        [
+          {
+            text: 'OK', onPress: () => {
+            }
+          },
+        ],
+        { cancelable: false }
+      )
+    }
+    else {
+      let location = await Location.getCurrentPositionAsync({});
+      this.setState({ location });
+    }
+  };
+
   componentDidMount() {
     this.props.getScooters();
   }
 
   render() {
     const { scooters } = this.props;
-    const markers = scooters.map(scooter => (
-      {
-        markerText: scooter.baterry.toString(),
-        title: scooter.serialCode.toString(),
-        description: scooter.serialCode.toString(),
-        coordinate: {
-          latitude: parseFloat(scooter.latitude),
-          longitude: parseFloat(scooter.longitude)
-        }
-      })
-    );
+    const { location } = this.state;
+    let markers = [];
+    if (location) {
+      markers = scooters.map(scooter => (
+        {
+          markerText: scooter.baterry.toString(),
+          title: `Serial Code: ${scooter.serialCode.toString()}`,
+          description: `Distance from you: ${geolib.getDistanceSimple({
+            latitude: location.coords.latitude,
+            longitude: location.coords.longitude
+          }, {
+            latitude: parseFloat(scooter.latitude),
+            longitude: parseFloat(scooter.longitude)
+          }).toString()} meters`,
+          coordinate: {
+            latitude: parseFloat(scooter.latitude),
+            longitude: parseFloat(scooter.longitude)
+          }
+        })
+      );
+    }
 
-    return (
-      <View>
-        <ScootersMapView
-          style={{
-            height: height,
-            backgroundColor: Colors.grey,
-            justifyContent: 'center',
-            alignItems: 'center'
-          }}
-          region={_.head(markers).coordinate}
-          markers={markers}
-        />
-      </View>
-    );
+    return _.isEmpty(markers) ? <ActivityIndicator/> :
+      (
+        <View>
+          <ScootersMapView
+            style={{
+              height: height,
+              backgroundColor: Colors.grey,
+              justifyContent: 'center',
+              alignItems: 'center'
+            }}
+            region={_.isEmpty(markers) ? null : _.head(markers).coordinate}
+            markers={markers}
+          />
+        </View>
+      );
   }
 }
 
